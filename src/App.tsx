8706c025da4eb5736f2e6bd18c7ab62c96a5e1af
skmtrd <skmtgame23@gmail.com>
2025-05-12 00:46:15 +0900

    typecheck and check command

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,20 @@
+import { useState } from "react";
+
 function App() {
+  const [count, setCount] = useState<number>(0);
+
   return (
-    <>
-      <h1 className="flex p-4 font-bold text-3xl">Hello World</h1>
-    </>
+    <div className="flex h-screen flex-col items-center justify-center">
+      <div className="p-4 font-bold text-3xl">Hello World</div>
+      <button
+        type="button"
+        className="rounded-md bg-blue-500 p-2 text-white"
+        onClick={() => setCount((prev) => prev + 1)}
+      >
+        Click me
+      </button>
+      <div>Count: {count}</div>
+    </div>
   );
 }
 
