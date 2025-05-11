7a5260fb65f67dada4bd8ba09e679ce55838b059
skmtrd <skmtgame23@gmail.com>
2025-05-12 01:01:44 +0900

    cicd settings

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,9 @@ import { defineConfig } from "vite";
 
 // https://vite.dev/config/
 export default defineConfig({
+  base:
+    process.env.GITHUB_REPOSITORY !== undefined
+      ? `/${process.env.GITHUB_REPOSITORY.split("/")[1]}`
+      : "",
   plugins: [react(), tailwindcss()],
 });
